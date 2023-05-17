83df3f3412203e6813ab935b231a9ef39011673d
VITOR ADRIEL ROECKER <vitor.roecker@catolicasc.edu.br>
2023-05-17 20:30:18 -0300

    Adicionado script de previsão e tratativa de dados

--- data_scraping.py
+++ data_scraping.py
@@ -2,6 +2,9 @@ import re
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
 page = requests.get("https://www.vivareal.com.br/venda/santa-catarina/joinville/apartamento_residencial")
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -43,3 +46,24 @@ for item in items:
 df = pd.DataFrame(data)
 
 df.head()
+
+
+X = df[['Rooms', 'm²']]
+y = df['Price']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.37, random_state=42)
+
+model = LinearRegression()
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+print("Mean Squared Error:", mse)
+
+new_data = {'Rooms': [2], 'm²': [60]}
+new_df = pd.DataFrame(new_data)
+
+prediction = model.predict(new_df)
+print("Predicted Price:", prediction)
